19ea38a2b6db46990f42d6c6da7dd05fdc7092a6
Desalegn Tamirat <desalegntamirateshetu@gmail.com>
2026-02-14 22:52:39 +0300

    Auth: implement login and register basics

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,58 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
+
+  async register(data: any) {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        name: data.name,
+        email: data.email,
+        password: hashedPassword,
+      },
+    });
+
+    return this.generateTokens(user.id, user.email);
+  }
+
+  async login(data: any) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: data.email },
+    });
+
+    if (!user) throw new UnauthorizedException('Invalid credentials');
+
+    const passwordMatches = await bcrypt.compare(data.password, user.password);
+
+    if (!passwordMatches)
+      throw new UnauthorizedException('Invalid credentials');
+
+    return this.generateTokens(user.id, user.email);
+  }
+
+  async generateTokens(userId: string, email: string) {
+    const accessToken = await this.jwt.signAsync(
+      {
+        sub: userId,
+        email,
+      },
+      { secret: process.env.JWT_SECRET, expiresIn: '15m' },
+    );
+
+    const refreshToken = await this.jwt.signAsync(
+      { sub: userId, email },
+      { secret: process.env.JWT_REFRESH_SECRET, expiresIn: '7d' },
+    );
+
+    return { accessToken, refreshToken };
+  }
+}
