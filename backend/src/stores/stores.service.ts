212e3b2a48fee78d7fdedbe3908eed95a140becb
Desalegn Tamirat <desalegntamirateshetu@gmail.com>
2026-02-16 11:03:36 +0300

    Implement Stores Services => creating and getting store

--- backend/src/stores/stores.service.ts
+++ backend/src/stores/stores.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateStoreDto } from './dto/create-store.dto';
+import { JwtPayload } from 'src/auth/interfaces/request-with-user.interface';
 
 @Injectable()
-export class StoresService {}
+export class StoresService {
+  constructor(private prisma: PrismaService) {}
+
+  async createStore(user: JwtPayload, createStoreDto: CreateStoreDto) {
+    if (user.role !== 'VENDOR') {
+      throw new ForbiddenException('Only vendors can create stores');
+    }
+
+    return this.prisma.store.create({
+      data: {
+        name: createStoreDto.name,
+        description: createStoreDto.description,
+        vendorId: user.sub,
+      },
+    });
+  }
+
+  async getMyStores(user: JwtPayload) {
+    return this.prisma.store.findMany({
+      where: { vendorId: user.sub },
+    });
+  }
+
+  async getAllStores() {
+    return this.prisma.store.findMany({
+      include: { vendor: true },
+    });
+  }
+}
